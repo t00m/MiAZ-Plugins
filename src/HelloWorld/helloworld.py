273b4111f0be6ac423315c6e737944024d2436c8
Tomás Vírseda <tomasvirseda@gmail.com>
2025-05-05 19:46:33 +0200

    Fix HelloWorld register method parameters

--- src/HelloWorld/helloworld.py
+++ src/HelloWorld/helloworld.py
@@ -35,7 +35,7 @@ class HelloWorld(GObject.GObject, Peas.Activatable):
         self.plugin = MiAZPlugin(self.app)
 
         ## Initialize plugin
-        self.plugin.register(self.file, self, self)
+        self.plugin.register(self.file, self)
 
         ## Get logger
         self.log = self.plugin.get_logger()
